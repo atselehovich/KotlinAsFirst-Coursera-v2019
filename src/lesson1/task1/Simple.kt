1fe7ced4d599f29775cbbc4017e8447c71914f18
PaniAnna <atselehovich@yandex.ru>
2021-11-09 20:47:51 +0300

    В KotlinAsFirst-Coursera-v2019\src\lesson1\task1\Simple.kt выполены задания lengthInMeters, thirdDigit, travelMinutes, accountInThreeYears.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -33,7 +33,7 @@ fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
 fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
     (-b + sqrt(discriminant(a, b, c))) / (2 * a)
 
-/**
+/*
  * Пример
  *
  * Поиск произведения корней квадратного уравнения
@@ -49,8 +49,15 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    println("${sqr(7)}")
+
+    println("${lengthInMeters(8, 2, 11)}")
+
+    println("${thirdDigit(3801)}")
+
+    println("${travelMinutes(1, 49, 20, 15)}")
+
+    println("${accountInThreeYears(1000, 5)}")
 }
 
 /**
@@ -59,16 +66,9 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minutes * 60 + seconds
+val passed = seconds(10, 10, 10)
 
-/**
- * Тривиальная
- *
- * Пользователь задает длину отрезка в саженях, аршинах и вершках (например, 8 саженей 2 аршина 11 вершков).
- * Определить длину того же отрезка в метрах (в данном случае 18.98).
- * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
- */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
 
 /**
  * Тривиальная
@@ -87,12 +87,39 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
 fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
 
 /**
+ * Тривиальная
+ *
+ * Пользователь задает длину отрезка в саженях, аршинах и вершках (например, 8 саженей 2 аршина 11 вершков).
+ * Определить длину того же отрезка в метрах (в данном случае 18.98).
+ * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
+ */
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    var vershokInMeters = vershoks * 0.04445
+    var arshinsInMeters = arshins * 16 * 0.04445
+    var sagenesInMeters = sagenes * 48 * 0.04445
+    return vershokInMeters + arshinsInMeters + sagenesInMeters
+}
+
+/**
  * Простая
  *
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    var tensThousands: Int
+    var numberTensThousands: Int
+    var thousands: Int
+    var numberThousands: Int
+    var hundreds: Int
+
+    tensThousands = number / 10000
+    numberTensThousands = number - tensThousands * 10000
+    thousands = numberTensThousands / 1000
+    numberThousands = numberTensThousands - thousands * 1000
+    hundreds = numberThousands / 100
+    return hundreds
+}
 
 /**
  * Простая
@@ -101,7 +128,11 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+
+    return (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
+
+}
 
 /**
  * Простая
@@ -110,7 +141,17 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    var firstYearFinal: Double
+    var percentDouble: Double
+    percentDouble = percent.toDouble()
+    var initialDouble: Double
+    initialDouble = initial.toDouble()
+    firstYearFinal = initialDouble * percentDouble / 100 + initialDouble
+    var secondYearFinal: Double
+    secondYearFinal = firstYearFinal * percentDouble / 100 + firstYearFinal
+    return secondYearFinal * percentDouble / 100 + secondYearFinal
+}
 
 /**
  * Простая
